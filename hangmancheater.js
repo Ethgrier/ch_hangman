b04051e474374a06b6a2152ac0daecc9f11182aa
Ethgrier <69974220+Ethgrier@users.noreply.github.com>
2020-08-20 19:20:52 +0200

    Add files via upload

--- hangmancheater.js
+++ hangmancheater.js
@@ -1,20 +1,26 @@
-let letters = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
+let letters = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+let words = ["banana","excess","giggit","voodoo","pullup","rhythm","myrrhy"];
+let guessString = "";
 
-for (i = 0; i < letters.length; i++){
-  let li =letters[i];
-    let a = "<span onClick=\"letterClick('"+li+"')\">"+li+"</span>";
-    if(li=="P"||li=="L"){
-      a+="</br>"
-    }
-    let kba = document.getElementById("keyboard").innerHTML;
-    document.getElementById("keyboard").innerHTML = kba + a;
+window.onload = setKeyboard();
+
+function setKeyboard(){
+  let kba = ""
+  for (i = 0; i < letters.length; i++){
+    let li =letters[i];
+      let a = "<span onClick=\"letterClick('"+li+"')\">"+li+"</span>";
+      if(li=="P"||li=="L"){
+        a+="</br>"
+      }
+      kba = kba + a;
+  }
+  document.getElementById("keyboard").innerHTML = kba
 }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-let words = ["banana","excess","giggit","voodoo","pullup","rhythm","myrrhy"]
 function getword(rexp){
   let re = new RegExp("["+rexp.toLowerCase()+"]");
   for (i = getRandomInt(words.length-1); i < words.length*3; i++){
@@ -22,14 +28,19 @@ function getword(rexp){
     //console.log(j);
     if (!re.test(words[j])){
       let sol = words[j].toUpperCase();
-      document.getElementById("solution").innerHTML = sol[0]+" "+sol[1]+" "+sol[2]+" "+sol[3]+" "+sol[4]+" "+sol[5];
+
       return words[j]
     }
   }
   return null
 }
 
-let guessString = "";
+function resetClick(){
+  guessString = "";
+  document.getElementById("solution").innerHTML = "_ _ _ _ _ _";
+  document.getElementById("guesses").innerHTML = "*****";
+  setKeyboard();
+}
 
 function letterClick(letter){
   if(guessString.length<5){
@@ -37,8 +48,9 @@ function letterClick(letter){
     showString = guessString + ("*".repeat(5-guessString.length))
     document.getElementById("guesses").innerHTML = showString;
     if(guessString.length==5){
-      getword(guessString);
-      document.getElementById("keyboard").innerHTML = "<div class=\"retry\" onClick=\"window.location.reload();\">You lost</br>Try again</div>"
+      sol = getword(guessString);
+      document.getElementById("solution").innerHTML = sol[0]+" "+sol[1]+" "+sol[2]+" "+sol[3]+" "+sol[4]+" "+sol[5];
+      document.getElementById("keyboard").innerHTML = "<div class=\"retry\" onClick=\"resetClick();\">You lost</br>Try again</div>"
     }
   }
 }
